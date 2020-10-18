f8f342ec00e0e9a55956821f0c920b2555ae1f1b
Wassim <mohamedwassil.beldjillali@gmail.com>
2020-10-18 21:44:38 +0200

    route modified

--- app/routes/signup.js
+++ app/routes/signup.js
@@ -26,32 +26,33 @@ router.post('/', async function signupHandler(req, res, next) {
     if (login.length === 0){
       notifier.notify('Veuillez renseigné un Username Valide')
       res.redirect('/signup');
-      return;
+      next();
     }
     if (password !== passwordConfirm){
       notifier.notify('Les mots de passe rensegnés ne sont pas identiques');
       res.redirect('/signup');
-      return;
+      next();
     }
     const usernameExist = await checkUsernameExist(login)
     if(usernameExist === true) {
         notifier.notify("Un compte assicié à ce username exist déja ! Veuillez renouveller votre inscription en choissisant un nouveau login :) ")
         res.redirect('/signup');
-        return;
+        next();
     }
 
     const emailexist = await checkEmailExist(req.body.email)
+    console.log(emailexist)
     if(emailexist === true) {
         notifier.notify("Un compte assicié à cet email exist déja !, Veuillez renouveler votre inscription avec un nouveau Mail")
         res.redirect('/signup');
-        return;
+        next();
     }
     passwordValidation(req.body.password)
+    .catch(response=>{console.log(response)})
     .catch(e => {
         notifier.notify(" Password faible ! Utiliser 8 caractères minimum dont une lettre miniscule, une lettre majuscule, un caractère spécial, et un chiffre")
         console.log(e);
         res.redirect('/signup');
-        
     });
     // const captchavalid = await captchaVerification(captcha)
     // if(!captchavalid){
@@ -60,7 +61,7 @@ router.post('/', async function signupHandler(req, res, next) {
     // sendMail({mail : email}) 
     const code = await generateRandomCode();
 
-    const link = `${process.env.SERVER_IP}:${process.env.PORT}/signup/confirm?code=${code}`;
+    const link = `/signup/confirm?code=${code}`;
     const html = `<p>Appuyez <a href="${link}"> ici </a> pour confirmer votre adresse mail.</p>`
     sendMail({mail : email , html})
     .catch(()=>{
