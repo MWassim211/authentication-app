2d0700d8cd765aa683b299a505f4ee32595df5fe
Wassim <mohamedwassil.beldjillali@gmail.com>
2020-10-18 19:59:17 +0200

    captcha validation reviewed

--- app/services/captchVerificationService.js
+++ app/services/captchVerificationService.js
@@ -4,8 +4,10 @@ module.exports.captchaVerification =  async function captchaVerification( captch
     
     const captchaSecretKey = process.env.CAPTCHA_SECRET
     const verificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${captchaSecretKey}&response=${captcha}`;
-
+        console.log(verificationUrl)
         const res =  await fetch(verificationUrl)
                     .then(response => response.json())
+                    .catch(err => console.log(err));
+            console.log(res);
         return res.success;
 }
